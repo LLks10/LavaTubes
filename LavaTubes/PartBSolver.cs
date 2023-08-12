5f2ff53949394a930318b15332f46da26de874cf
LLks10 <lukasvdl10@gmail.com>
2023-08-12 19:07:07 +0200

    cleanup

--- LavaTubes/PartBSolver.cs
+++ LavaTubes/PartBSolver.cs
@@ -1,31 +1,6 @@
 ﻿namespace LavaTubes;
 
-internal class PartASolver
-{
-	public static int CalculateRiskLevel(in Field field)
-	{
-		int count = 0;
-
-		for (int y = 0; y < field.Height; y++)
-			for (int x = 0; x < field.Width; x++)
-			{
-				char curr = field[x, y];
-				if (curr < field.GetBounded(x + 1, y) &&
-					curr < field.GetBounded(x - 1, y) &&
-					curr < field.GetBounded(x, y + 1) &&
-					curr < field.GetBounded(x, y - 1))
-				{
-					count += curr - '0' + 1;
-					
-					x++; // Two adjacent lowpoints is impossible
-				}
-			}
-
-		return count;
-	}
-}
-
-internal class PartBSolver
+sealed internal class PartBSolver
 {
 	const char WALL = '9';
 
