2527bc34b8b5bf313dfbff36cfc9debe217c7d03
LLks10 <lukasvdl10@gmail.com>
2023-08-12 17:24:42 +0200

    Cleanup

---
+++ Field.cs
@@ -0,0 +1,50 @@
+﻿namespace LavaTubes;
+
+sealed class Field
+{
+	const char BOUNDARY = (char)('9' + 1);
+	private readonly IReadOnlyList<char> field;
+
+	private Field(List<char> field, int width)
+	{
+		this.field = field;
+		Width = width;
+		Height = field.Count / width;
+	}
+
+	public int Width { get; init; }
+	public int Height { get; init; }
+
+	public static Field CreateFromFile(string filename)
+	{
+		using var sr = new StreamReader(filename);
+
+		var field = new List<char>();
+		var line = sr.ReadLine();
+		if (line is null)
+			return new Field(field, 0);
+
+		int width = line.Length;
+
+		while (line is not null)
+		{
+			field.AddRange(line);
+			line = sr.ReadLine();
+		}
+
+		return new Field(field, width);
+	}
+
+	public char this[int x, int y] => field[x + y * Width];
+
+	public bool InBounds(int x, int y) =>
+		x >= 0 && y >= 0 && x < Width && y < Height;
+
+	public char GetBounded(int x, int y)
+	{
+		if (!InBounds(x, y))
+			return BOUNDARY;
+
+		return this[x, y];
+	}
+}
---
+++ Solver.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace LavaTubes;
+
+sealed class Solver
+{
+	public static int CalculateRiskLevel(Field field)
+	{
+		int count = 0;
+
+		for (int y = 0; y < field.Height; y++)
+		{
+			for (int x = 0; x < field.Height; x++)
+			{
+				char curr = field[x, y];
+				if (curr < field.GetBounded(x + 1, y) &&
+					curr < field.GetBounded(x - 1, y) &&
+					curr < field.GetBounded(x, y + 1) &&
+					curr < field.GetBounded(x, y - 1))
+				{
+					count += curr.GetValue() + 1;
+				}
+			}
+		}
+
+		return count;
+	}
+}
+
+static class CharExtensions
+{
+	public static int GetValue(this char c) => c - '0';
+}
