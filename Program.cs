97698609ca3517bc766bf61a43e28adee7e96c81
LLks10 <lukasvdl10@gmail.com>
2023-08-12 15:49:26 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using (var sw = new StreamReader("../../../input.txt"))
+{
+	var data = sw.ReadToEnd();
+	Console.WriteLine(data);
+}
+
+Console.WriteLine("Hello, World!");
